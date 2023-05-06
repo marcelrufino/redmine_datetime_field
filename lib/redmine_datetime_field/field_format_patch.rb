68554ea65a7c6daf5672ebe6f2a06d94f789b961
Marcel Rufino de Carvalho <marcelrufino@gmail.com>
2023-05-06 18:47:38 -0300

    Update

--- lib/redmine_datetime_field/field_format_patch.rb
+++ lib/redmine_datetime_field/field_format_patch.rb
@@ -27,6 +27,7 @@ module RedmineDatetimeField
       end
 
       def query_filter_options(custom_field, query)
+        query.operators_by_filter_type << :datetime => [ "=", ">=", "<=", "><", "<t+", ">t+", "><t+", "t+", "nd", "t", "ld", "nw", "w", "lw", "l2w", "nm", "m", "lm", "y", ">t-", "<t-", "><t-", "t-", "!*", "*" ]
         { :type => :datetime }
       end
 
