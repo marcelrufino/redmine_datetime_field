fe5a8914e0fcdfc047c7a464717e31c6b592b706
Marcel Rufino de Carvalho <marcelrufino@gmail.com>
2023-05-06 20:19:14 -0300

    Update

--- lib/redmine_datetime_field/query_patch.rb
+++ lib/redmine_datetime_field/query_patch.rb
@@ -3,7 +3,6 @@ module RedmineDatetimeField
         def self.included(base)            
             base.class_eval do
                 unloadable
-                logger.info "**** PASSOU AQUI ****"
                 self.operators_by_filter_type[:datetime] = [ "=", ">=", "<=", "><", "<t+", ">t+", "><t+", "t+", "nd", "t", "ld", "nw", "w", "lw", "l2w", "nm", "m", "lm", "y", ">t-", "<t-", "><t-", "t-", "!*", "*" ]
             end # do
         end # def
