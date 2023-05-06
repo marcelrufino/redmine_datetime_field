5f021d65ae38c09f14903eb7e1bfb185546e33aa
Marcel Rufino de Carvalho <marcelrufino@gmail.com>
2023-05-06 19:32:13 -0300

    Update

--- init.rb
+++ init.rb
@@ -4,6 +4,11 @@ curr_dirname = File.dirname(__FILE__)
   require File.join(curr_dirname, "lib", "redmine_datetime_field", require_file)
 end
 
+logger.info "**** PASSOU AQUI ****"
+Query.operators_by_filter_type << :datetime => [ "=", ">=", "<=", "><", "<t+", ">t+", "><t+", "t+", "nd", "t", "ld", "nw", "w", "lw", "l2w", "nm", "m", "lm", "y", ">t-", "<t-", "><t-", "t-", "!*", "*" ]
+logger.info "**** QUERY: #{query.operators_by_filter_type}"
+
+
 Redmine::Plugin.register :redmine_datetime_field do
   name "Date Time Custom Field plugin"
   author "Dmitry Migunov"
@@ -12,5 +17,3 @@ Redmine::Plugin.register :redmine_datetime_field do
 
   Redmine::FieldFormat.__send__(:include, RedmineDatetimeField::FieldFormatPatch)
 end
-
-Query.operators_by_filter_type << :datetime => [ "=", ">=", "<=", "><", "<t+", ">t+", "><t+", "t+", "nd", "t", "ld", "nw", "w", "lw", "l2w", "nm", "m", "lm", "y", ">t-", "<t-", "><t-", "t-", "!*", "*" ]
