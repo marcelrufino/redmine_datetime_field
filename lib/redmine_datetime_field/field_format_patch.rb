50d33fac6202b01db3fa8d98d15bac2fca23d49a
Marcel Rufino de Carvalho <marcelrufino@gmail.com>
2023-05-06 16:13:21 -0300

    Update

--- lib/redmine_datetime_field/field_format_patch.rb
+++ lib/redmine_datetime_field/field_format_patch.rb
@@ -38,7 +38,7 @@ module RedmineDatetimeField
         res = ""
 
         unless !value || value.empty?
-          res = Time.current.strftime("%d-%m-%YT%H:%M")
+          res = DateTime.parse(value).strftime("%d-%m-%YT%H:%M")
         end
 
         if html
