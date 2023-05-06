1bbbde1acfacc7d0dcf78733562ca283c4aa3acc
Marcel Rufino de Carvalho <marcelrufino@gmail.com>
2023-05-06 16:39:33 -0300

    Update

--- lib/redmine_datetime_field/field_format_patch.rb
+++ lib/redmine_datetime_field/field_format_patch.rb
@@ -18,7 +18,7 @@ module RedmineDatetimeField
       end
 
       def edit_tag(view, tag_id, tag_name, custom_value, options = {})
-        view.datetime_field_tag(tag_name, custom_value.value, options.merge(:id => tag_id, :size => 10))
+        view.datetime_field_tag(tag_name, DateTime.parse(custom_value.value).strftime("%d-%m-%YT%H:%M"), options.merge(:id => tag_id, :size => 10))
       end
 
       def bulk_edit_tag(view, tag_id, tag_name, custom_field, objects, value, options = {})
